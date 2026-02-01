e5389e7bcef997ee086fe8edc0ef9908e9856ed0
ju-ro <juro86@gmail.com>
2026-02-01 22:34:54 +0100

    update

--- tests/demo.spec.ts
+++ tests/demo.spec.ts
@@ -2,35 +2,40 @@ import { test, expect } from '@playwright/test';
 import { LOGIN_URL, USER_NAME, USER_PASSWORD } from './constants';
 import { loginWithSavingCookies } from './functions';
 
-test('User Authentification', async ({ page }) => {
+test('Autentifikácia užívateľa s uložením cookies', async ({ page }) => {
   await loginWithSavingCookies(page, LOGIN_URL, USER_NAME, USER_PASSWORD);
 });
 
-test.describe('demo', () => {
+test.describe('Demo test suite: ', () => {
   test.use({storageState: 'tests/.auth/session.json'})
-    test('Overenie prihláseného uživateľa', async ({ page }) => {
+
+    test('Overenie úspešne prihláseného uživateľa', async ({ page }) => {
         await page.goto(LOGIN_URL);
         const odhlasitButton = page.locator('.logout.hidden-sm-down');
         await expect.soft(odhlasitButton).toBeVisible({timeout : 2000});
     });
 
-    test('Zmena jazyka', async ({ page }) => {
+    test('Zmena jazyka e-shopu', async ({ page }) => {
         await page.goto(LOGIN_URL);
         const language = page.locator('xpath=//*[@id="_desktop_language_selector"]/div/div/button/span');
-        
-        try {
-          await expect(language).toHaveText('Čeština');
+        const zistiJazyk = await language.innerText();
+
+        if (zistiJazyk.includes('Čeština')) {
           console.log('Nastavená CS, idem zmeniť jazyk na EN');
           await page.locator('[id="_desktop_language_selector"]').click();
           await page.getByRole('link', { name: 'English' }).click();
-        } catch {
+          await expect(language).toContainText('English', { timeout: 500 });
+          console.log('Zmena jazyka na angličtinu bola úspešná.');
+        } else {
           console.log('Nastavená EN, idem zmeniť jazyk na CS');
           await page.locator('[id="_desktop_language_selector"]').click();
           await page.getByRole('link', { name: 'Čeština' }).click();
+          await expect(language).toContainText('Čeština', { timeout: 500 });
+          console.log('Zmena jazyka na češtinu bola úspešná.');
         }
     });
 
-    test('Zmena meny', async ({ page }) => {
+    test('Zmena meny používanej v e-shope', async ({ page }) => {
         await page.goto(LOGIN_URL);
         const overMenu = page.locator('.expand-more._gray-darker');
         const text = await overMenu.innerText();
@@ -60,8 +65,39 @@ test.describe('demo', () => {
           await ceskaKoruna.click();
           await expect(overMenu).toContainText('CZK Kč', { timeout: 500 });
         }
-
     });
+
+    test('Overenie úspešného vloženia položky do košíka', async ({ page }) => {
+        await page.goto(LOGIN_URL);
+        const kosik = page.locator('.cart-products-count').first();
+        
+        const getCartCount = async () => {
+          const text = await kosik.innerText();
+          return Number(text.replace(/[()]/g, ''));
+        };
+
+        const stavPred = await getCartCount();
+        console.log('Aktuálny stav položiek v nákupnom košíku je:', stavPred);
+
+        const prvaPolozkaNaEshope = page.locator('[data-id-product="1"]').first();
+        const pridajDoKosika = page.getByRole('button', { name: ' Přidat do košíku' });
+        const produktUspesnePridany = page.getByRole('heading', { name: ' Produkt byl úspěšně přidán' })
+
+        await prvaPolozkaNaEshope.click();
+        await expect(pridajDoKosika).toBeVisible({timeout : 3000});
+        await pridajDoKosika.click();
+
+        await expect(produktUspesnePridany).toBeVisible({timeout : 3000});
+        await page.getByRole('button', { name: 'Pokračovat v nákupu' }).click();
+
+        await expect(kosik).not.toHaveText(`(${stavPred})`);
+
+        const stavPo = await getCartCount();
+        console.log('Košík po:', stavPo);
+
+        expect(stavPo).toBe(stavPred + 1);
+    });
+
 
 });
 
